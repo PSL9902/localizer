c4e718a7426991989075feb21ef19265d2908fbc
Pavel <reeqween@gmail.com>
2021-04-07 10:22:19 +0600

    ...

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 //!Simple library for localization.
 
-//!# no unsafe code
+//! [![Current Crates.io Version](https://img.shields.io/crates/v/localizer.svg)](https://crates.io/crates/localizer)
+//! [![Document](https://img.shields.io/badge/doc-localizer-green.svg)](https://docs.rs/localizer)
+//! # no unsafe code
 //! # Example:
 //! ```
 //! use localizer::*;
@@ -18,6 +20,24 @@
 //! let loc = Localizer::create().file(None).current_lang(Some("ru".to_string()));
 //! println!("{:?}", loc.get(&"ex1"));
 //! ```
+//! 
+//! 
+//! # Further examples:
+//! > cargo run --example %example%
+//! ## 1:
+//! [ex1](https://github.com/PSL9902/localizer/blob/master/examples/ex1.rs)
+//! ## 2:
+//! [ex2](https://github.com/PSL9902/localizer/blob/master/examples/ex2.rs)
+//! ## 3:
+//! [Tic_Tac_Toe game1](https://github.com/PSL9902/localizer/blob/master/examples/tic-tac-toe.rs)
+//! [Tic_Tac_Toe game2](https://github.com/PSL9902/rust_Tic_Tac_Toe/tree/master)
+//! # No-Std:
+//! > feature = "no_std"
+//! ## Cargo.toml:
+//! > localizer = {version = ..., default-features = false, features = ["no_std"]}
+
+
+
 pub mod constants;
 
 pub mod structs;
